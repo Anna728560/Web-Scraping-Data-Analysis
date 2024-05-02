ecab233abd29aecaae39f3cd7b19d86c2cd803aa
Anya <xxxuko@gmail.com>
2024-05-02 12:44:20 +0300

    Add functionality to scrape posted_date from job detail pages

--- scrapy_project/scrapy_project/spiders/tch.py
+++ scrapy_project/scrapy_project/spiders/tch.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import re
+
 from scrapy import Selector
 from scrapy.http import Response
 import scrapy
@@ -23,7 +26,8 @@ class TchSpider(scrapy.Spider):
             yield {
                 "name": tech.css("h2>a::text").get(),
                 "company": tech.css(".add-top-xs>span::text").get(),
-                "technologies": self.parse_technologies_block(response, tech)
+                "technologies": self.parse_technologies_block(response, tech),
+                "posted_date": self.extract_posted_date(response, tech),
             }
 
     def parse_technologies_block(self, response: Response, tech: Selector):
@@ -39,5 +43,14 @@ class TchSpider(scrapy.Spider):
             technology_list.append(technology.text)
 
         return technology_list
+
+    def extract_posted_date(self, response: Response, tech: Selector):
+        detailed_url = tech.css("h2>a::attr(href)").get()
+        self.driver.get(response.urljoin(detailed_url))
+
+        posted_date_element = self.driver.find_element(By.CLASS_NAME, "text-default-7")
+        posted_date_text = posted_date_element.text.replace("Вакансія від ", "")
+
+        return posted_date_text
 
 
