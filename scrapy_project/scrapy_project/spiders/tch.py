4bebd41f06479960c7b3f4c5b38fab0c572a707f
Anya <xxxuko@gmail.com>
2024-05-02 12:07:37 +0300

    Add functionality to scrape technologies from job detail pages

--- scrapy_project/scrapy_project/spiders/tch.py
+++ scrapy_project/scrapy_project/spiders/tch.py
@@ -1,6 +1,8 @@
+from scrapy import Selector
 from scrapy.http import Response
 import scrapy
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class TchSpider(scrapy.Spider):
@@ -21,5 +23,21 @@ class TchSpider(scrapy.Spider):
             yield {
                 "name": tech.css("h2>a::text").get(),
                 "company": tech.css(".add-top-xs>span::text").get(),
+                "technologies": self.parse_technologies_block(response, tech)
             }
+
+    def parse_technologies_block(self, response: Response, tech: Selector):
+        detailed_url = tech.css("h2>a::attr(href)").get()
+        self.driver.get(response.urljoin(detailed_url))
+
+        technologies_block = self.driver.find_element(By.CLASS_NAME, "toggle-block")
+        technologies = technologies_block.find_elements(By.CLASS_NAME, "label-skill")
+
+        technology_list = []
+
+        for technology in technologies:
+            technology_list.append(technology.text)
+
+        return technology_list
+
 
