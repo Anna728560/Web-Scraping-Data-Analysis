4fe015f55fb7860fb4903adf00363363bcb13c70
Anya <xxxuko@gmail.com>
2024-05-02 11:09:24 +0300

    Add TchSpider to scrape company name and title of jobs from work.ua(first page)

--- scrapy_project/scrapy_project/spiders/tch.py
+++ scrapy_project/scrapy_project/spiders/tch.py
@@ -1,4 +1,6 @@
+from scrapy.http import Response
 import scrapy
+from selenium import webdriver
 
 
 class TchSpider(scrapy.Spider):
@@ -6,5 +8,18 @@ class TchSpider(scrapy.Spider):
     allowed_domains = ["www.work.ua"]
     start_urls = ["https://www.work.ua/jobs-python/"]
 
-    def parse(self, response):
-        pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
+
+    def close(self, reason):
+        self.driver.close()
+
+    def parse(self, response: Response, **kwargs):
+
+        for tech in response.css(".card-hover"):
+            yield {
+                "name": tech.css("h2>a::text").get(),
+                "company": tech.css(".add-top-xs>span::text").get(),
+            }
+
